db212d66e6455c12cac59133c4e8addd98b522e3
LaurentSewer <Laurent.Sewer@formation.orif.ch>
2018-06-19 10:49:37 +0200

    add min 1 for all numeric champ
    empèche de donner des valeures négatives ou 0 points et passer le nombre de réponces attendue par un integer pour ne pas avoir de valeur non-entière

--- application/controllers/Question.php
+++ application/controllers/Question.php
@@ -436,7 +436,7 @@ class Question extends MY_Controller
 
 		if (isset($_POST['save'])){
 			$this->form_validation->set_rules('name', $this->lang->line('question_text'), 'required');
-			$this->form_validation->set_rules('points', $this->lang->line('points'), 'required|numeric');
+			$this->form_validation->set_rules('points', $this->lang->line('points'), 'required|numeric|greater_than[0]');
 			for($i=0; $i < $_POST['nbAnswer']; $i++){
 				$this->form_validation->set_rules('reponses['.$i.'][question]', $this->lang->line('answers_list'), 'required');
 				$this->form_validation->set_rules('reponses['.$i.'][answer]', $this->lang->line('valid_answer'), 'required');
@@ -617,8 +617,8 @@ class Question extends MY_Controller
 
 		if (isset($_POST['save'])){
 			$this->form_validation->set_rules('name', $this->lang->line('question_text'), 'required');
-			$this->form_validation->set_rules('points', $this->lang->line('points'), 'required|numeric');
-			$this->form_validation->set_rules('nb_desired_answers', $this->lang->line('nb_desired_answers'), 'required|numeric');
+			$this->form_validation->set_rules('points', $this->lang->line('points'), 'required|numeric|greater_than[0]');
+			$this->form_validation->set_rules('nb_desired_answers', $this->lang->line('nb_desired_answers'), 'required|integer|greater_than[0]');
 			for($i=0; $i < $_POST['nbAnswer']; $i++){
 				$this->form_validation->set_rules('reponses['.$i.'][answer]', $this->lang->line('answers_list'), 'required');
 			}
@@ -793,7 +793,7 @@ class Question extends MY_Controller
 
 		if (isset($_POST['save'])){
 			$this->form_validation->set_rules('name', $this->lang->line('question_text'), 'required');
-			$this->form_validation->set_rules('points', $this->lang->line('points'), 'required|numeric');
+			$this->form_validation->set_rules('points', $this->lang->line('points'), 'required|numeric|greater_than[0]');
 			$this->form_validation->set_rules('cloze_text', $this->lang->line('text'), 'required');
 			for($i=0; $i < $_POST['nbAnswer']; $i++){
 				$this->form_validation->set_rules('reponses['.$i.'][answer]', $this->lang->line('answers_list'), 'required');
@@ -985,7 +985,7 @@ class Question extends MY_Controller
 			if (isset($_POST['save'])){
 				$_SESSION['filtres'] = "Question?module=&topic=&type=";
 				$this->form_validation->set_rules('name', $this->lang->line('question_text'), 'required');
-				$this->form_validation->set_rules('points', $this->lang->line('points'), 'required|numeric');
+				$this->form_validation->set_rules('points', $this->lang->line('points'), 'required|numeric|greater_than[0]');
 				$this->form_validation->set_rules('answer', $this->lang->line('answer_question_add'), 'required');
 				
 				if ($this->form_validation->run()){
@@ -1026,7 +1026,7 @@ class Question extends MY_Controller
 			if (isset($_POST['save'])){
 				$_SESSION['filtres'] = "Question?module=&topic=&type=";
 				$this->form_validation->set_rules('name', $this->lang->line('question_text'), 'required');
-				$this->form_validation->set_rules('points', $this->lang->line('points'), 'required|numeric');
+				$this->form_validation->set_rules('points', $this->lang->line('points'), 'required|numeric|greater_than[0]');
 				$this->form_validation->set_rules('answer', $this->lang->line('answer_question_add'), 'required');
 				
 				if ($this->form_validation->run()){
@@ -1155,7 +1155,7 @@ class Question extends MY_Controller
 				redirect('/Question');
 			} elseif (isset($_POST['save'])){
 				$this->form_validation->set_rules('name', $this->lang->line('question_text'), 'required');
-				$this->form_validation->set_rules('points', $this->lang->line('points'), 'required|numeric');
+				$this->form_validation->set_rules('points', $this->lang->line('points'), 'required|numeric|greater_than[0]');
 				for($i=0; $i < $_POST['nbAnswer']; $i++){
 					$this->form_validation->set_rules('reponses['.$i.'][symbol]', $this->lang->line('landmark'), 'required');
 					$this->form_validation->set_rules('reponses['.$i.'][answer]', $this->lang->line('answers_list'), 'required');
