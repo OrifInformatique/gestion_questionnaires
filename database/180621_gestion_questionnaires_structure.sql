618f91f4b255e06560071ffae2942c5077ed4416
Didier Viret <didier.viret@orif.ch>
2018-06-22 16:15:55 +0200

    Add ci_sessions database table
    Add ci_sessions table to manage sessions in the database.

--- database/180621_gestion_questionnaires_structure.sql
+++ database/180621_gestion_questionnaires_structure.sql
@@ -25,6 +25,19 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `ci_sessions`
+--
+
+CREATE TABLE `ci_sessions` (
+  `id` varchar(128) NOT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `timestamp` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `data` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `t_answer_distribution`
 --
 
