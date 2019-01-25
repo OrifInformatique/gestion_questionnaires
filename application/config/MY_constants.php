e3d632cb960c16cfc6ee4acd01a6ad916f596b03
Didier Viret <didier.viret@orif.ch>
2019-01-25 11:08:11 +0100

    Put application constants in separate file

---
+++ application/config/MY_constants.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| CUSTOM CONSTANTS
+|--------------------------------------------------------------------------
+|
+| These are constants defined specially for this application.
+|
+*/
+
+/*
+|--------------------------------------------------------------------------
+| Password and username min length
+|--------------------------------------------------------------------------
+*/
+defined('PASSWORD_MIN_LENGTH') OR define('PASSWORD_MIN_LENGTH', 6);
+defined('USERNAME_MIN_LENGTH') OR define('USERNAME_MIN_LENGTH', 2);
+
