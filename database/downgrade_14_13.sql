80fe95783c85c5420fd13154ec9353c2b8729f61
MeSa <42637780+MestourBS@users.noreply.github.com>
2020-05-29 13:50:59 +0200

    Remove database name and request 'REDIRECT_CI_ENV' for CI_ENV (#258)
    * Remove the database name in database scripts, to make them usable in local database and in hosting database.
    
    * Add the 'REDIRECT_' prefix to the CI_ENV environment variable, to make it usable in Infomaniak hosting.

--- database/downgrade_14_13.sql
+++ database/downgrade_14_13.sql
@@ -1,8 +1,8 @@
-RENAME TABLE `gestion_questionnaires`.`user` TO `gestion_questionnaires`.`t_user`;
+RENAME TABLE `user` TO `t_user`;
 ALTER TABLE `user` CHANGE `id` `ID` INT(11) NOT NULL AUTO_INCREMENT, CHANGE `fk_user_type` `FK_User_Type` INT(11) NOT NULL, CHANGE `username` `User` VARCHAR(45) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL, CHANGE `password` `Password` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL, CHANGE `archive` `Archive` TINYINT(4) NOT NULL DEFAULT '0';
 ALTER TABLE `user` DROP COLUMN `date_creation`;
 
-RENAME TABLE `gestion_questionnaires`.`user_type` TO `gestion_questionnaires`.`t_user_type`;
+RENAME TABLE `user_type` TO `t_user_type`;
 ALTER TABLE `user_type` CHANGE `id` `ID` INT(11) NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `t_question_type` ADD `Archive` BOOLEAN NOT NULL DEFAULT FALSE AFTER `Type_Name`;
